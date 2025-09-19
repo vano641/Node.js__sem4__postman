b4f5223ab799ba460487501621a459ee9988c933
Ivan Mysovskii <vano641@mail.ru>
2025-09-19 10:16:09 +0300

    Joi обработка входных с postman данных по шаблону

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+// Задание №6 (тайминг 10 минут)
+// - Установить Joi с помощью NPM (npm i joi)
+// - Определить схему валидации запроса на создание пользователя(пока не применять в
+// обработчиках)
+// - firstName и secondName - это строки, которые должны иметь не менее одного
+// символа. Также эти поля обязательны для создания.
+// - age - это обязательное число, которое не может быть меньше 0 и более 150
+// - city - это необязательная строка с минимальным количеством символов 1
+
 const express = require('express');
+const joi = require('joi');
+
 
 const app = express();
 
 const users = [];
-
 let uniqueId = 0;
 
+// "проверка(валидация) данных по описанным параметрам"
+const userSchema = joi.object({
+    // цепочка методов описывающие параметры
+    // тип строка, мин.длинна 1 символ, обязательное поле
+    firstName: joi.string().min(1).required(),
+    secondName: joi.string().min(1).required(),
+    age: joi.number().min(0).max(150).required(),
+    city: joi.string().min(1)
+})
 
 app.use(express.json())
 
@@ -40,6 +59,14 @@ app.post('/users', (req, res) => {
 
 // обновляем пользователя
 app.put('/users/:id', (req, res) => {
+    // результат валидации
+    const result = userSchema.validate(req.body);
+    // проверка (в Postman если в методе Обновления данных передать "Пустую строку
+    // в поле userName, то вывалится ошибка Не соответствия переданных данных параметрам валидации")
+    if (result.error) {
+        return res.status(404).send({error: result.error.details});
+    }
+
     const userId = Number(req.params.id); // или можно записать так +req.params.id приводим к number
     const user = users.find((user) => user.id === userId);
     if (user) {
